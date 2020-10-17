88691490d666792d8c9326f2a34c04625fb266af
sungyun <sungyun5423@gmail.com>
2020-10-17 20:06:03 +0900

    10/17

--- src/component/Axios.jsx
+++ src/component/Axios.jsx
@@ -14,10 +14,10 @@ const Users = () => {
             // loading 상태를 true 로 바꿉니다.
             setLoading(true);
 
-            const reponse = await axios.get(
-                'http://jsonplaceholder.typicode.com/users'
+            const response = await axios.get(
+                'https://jsonplaceholder.typicode.com/users'
             );
-            setUsers(reponse.data.filter(user => user.id === 1)); // 데이터는 response.data 안에 들어있습니다.
+            setUsers(response.data.filter(user => user.id === 2)); // 데이터는 response.data 안에 들어있습니다.
         } catch (e) {
             setError(e);
         }
@@ -28,8 +28,8 @@ const Users = () => {
         fetchUsers();
     }, []);
 
-    if (loading) return <div>로딩중..</div>;
-    if (error) return <div>에러가 발생했습니다.</div>;
+    if (loading) return <div>로딩 중...</div>;
+    if (error) return <div>에러가 발생했습니다!</div>;
     if (!users) return null;
 
     return (

abb877db8a550d41f684bdbcfb685c5911c69f53
sungyun <sungyun5423@gmail.com>
2020-11-03 00:27:30 +0900

    11/3

--- src/airbnb/component/Footer.js
+++ src/airbnb/component/Footer.js
@@ -1,12 +1,5 @@
 import React from 'react';
 import FooterStyle from '../style/FooterStyle';
-import globe from '../img/globe.png';
-import won from '../img/won.png';
-import facebook from '../img/facebook.png';
-import twitter from '../img/twitter.png';
-import instagram from '../img/instagram.png';
-import naverblog from '../img/naverblog.png';
-import naverpost from '../img/naverpost.png';
 
 const Footer = () => {
     return (
